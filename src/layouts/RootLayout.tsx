85b06f2f9d01e5eb0bac296d5e1930fe6fcb53aa
fitronansori <fitronansori7@gmail.com>
2024-10-17 11:58:19 +0800

    Add Header component to RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/layouts/Header";
 import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
