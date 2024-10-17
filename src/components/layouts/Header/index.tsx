73c3d22de83eb6faa8d2f8ce1928bf1a88e73547
fitronansori <fitronansori7@gmail.com>
2024-10-17 14:45:50 +0800

    Refactor Header component to add authentication functionality and logout button

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -1,7 +1,24 @@
+import { RootState } from "@/app/store";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "@/app/features/auth/authSlice";
+import { toast } from "react-toastify";
 
 const Header = () => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutBtn = () => {
+    dispatch(logout());
+    toast.success("Logged out successfully");
+    navigate("/auth/login");
+  };
+
   return (
     <header className="bg-primary">
       <div className="container h-16 flex items-center justify-between text-white">
@@ -10,9 +27,20 @@ const Header = () => {
         </Link>
 
         <div>
-          <Button variant={"secondary"} asChild>
-            <Link to="/auth/login">Login</Link>
-          </Button>
+          {isAuthenticated ? (
+            <div className="flex items-center gap-2">
+              <Button variant={"secondary"} asChild>
+                <Link to="/dashboard">Dashboard</Link>
+              </Button>
+              <Button variant={"destructive"} onClick={logoutBtn}>
+                Log Out
+              </Button>
+            </div>
+          ) : (
+            <Button variant={"secondary"} asChild>
+              <Link to="/auth/login">Login</Link>
+            </Button>
+          )}
         </div>
       </div>
     </header>
