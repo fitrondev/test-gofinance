f352fad48691499f9e1cde349582a82d00cad624
fitronansori <fitronansori7@gmail.com>
2024-10-17 15:58:19 +0800

    Add authentication redirect in AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { RootState } from "@/app/store";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const AuthLayout = () => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+  }, [isAuthenticated, navigate]);
+
   return (
     <>
       <Outlet />
