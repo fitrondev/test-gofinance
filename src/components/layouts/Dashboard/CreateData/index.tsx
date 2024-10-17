b237277145da05edab1d40f98b7de023310737e1
fitronansori <fitronansori7@gmail.com>
2024-10-17 21:07:07 +0800

    Add CreateData component to dashboard router

---
+++ src/components/layouts/Dashboard/CreateData/index.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import CreateDataForm from "./CreateDataForm";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
+
+const CreateData = () => {
+  return (
+    <section className="mt-10 h-[calc(100vh-104px)]">
+      <div className="container">
+        <div>
+          <Button asChild>
+            <Link to="/dashboard">
+              <ArrowLeft /> Back to Dashboard
+            </Link>
+          </Button>
+        </div>
+      </div>
+
+      <div className="container h-[calc(100%-40px)] flex items-center justify-center">
+        <CreateDataForm />
+      </div>
+    </section>
+  );
+};
+export default CreateData;
