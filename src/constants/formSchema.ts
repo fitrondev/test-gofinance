6a0daaeeac6ff0a559c249af849af482d2186f71
fitronansori <fitronansori7@gmail.com>
2024-10-17 11:36:16 +0800

    Add login and register form schemas

---
+++ src/constants/formSchema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const registerSchema = z.object({
+  username: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
